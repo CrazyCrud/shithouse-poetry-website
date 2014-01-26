9187b968a1b7fb013e7733fb83276cbe92b3f487
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-01-27 00:10:14 +0100

    Added Waypoint and Transit libraries and started implementing the top-bar navigation

--- js/global.js
+++ js/global.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 	$(document).foundation();
+	setupNavigation();
 });
+
+function setupNavigation(){
+	var $uploadLink = $("#link-upload");
+	$("#upload-button").waypoint(function(direction){
+		if(direction == "down"){
+			var $stickyElement = $uploadLink.clone(true);
+			$(".li-upload-container").append($stickyElement);
+			$stickyElement.css({'opacity': 0})
+						.animate({'opacity': 1.0}, 600);
+			$uploadLink.animate({'opacity': 0}, 300);
+		}else{
+			var $stickyElement = $(".li-upload-container > #link-upload");
+			$stickyElement.css({'opacity': 1.0})
+						.animate({
+							'opacity': 0}, 300, function() {
+								$(".li-upload-container").empty();
+						});
+			$uploadLink.animate({'opacity': 1.0}, 300);
+		}
+	});
+}
