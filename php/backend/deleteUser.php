dfed5b0a5c44d68a745c3b7f0a6941f55b81d4c6
thhesp <Thomas_spr@yahoo.de>
2014-02-17 15:46:32 +0100

    Extended Backend
    Added deleteUser.php

---
+++ php/backend/deleteUser.php
@@ -0,0 +1,61 @@
+<?php
+
+// How to use this page:
+// open it with the info about the course to create
+// as described in the database and your sessionkey:
+//
+// getUser.php?authkey=123
+//
+// required parameters are:
+// authkey
+//
+// The answer looks as follows:
+// a json with a successcode and the course id:
+/* 
+{
+	success : 1 ,
+	data : true
+}
+*/
+// for success codes see ../php/config.php
+
+// HEADER
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(0);
+include("../settings/config.php");
+include("../helpers/dbhelper.php");
+// END HEADER
+
+// PREPARE RESULT
+$json = array();
+$json["success"]=$CODE_INSUFFICIENT_PARAMETERS;
+
+if(isset($_GET["authkey"])){
+	$key = $_GET["authkey"];
+}else{
+	$json["message"]="authkey missing";
+	echo json_encode($json);
+	exit();
+}
+
+$db = new DBHelper();
+$db->setAuthKey($key);
+$user = $db->deleteUser();
+
+if($user == false){
+	$json["success"]=$CODE_ERROR;
+	if(DBConnection::getInstance()->status == DBConfig::$dbStatus["offline"]){
+		$json["message"] = "Database error";
+	}else{
+		$json["message"] = "User not found";
+	}
+	echo json_encode($json);
+	exit();
+}
+
+$json["data"] = $user;
+
+$json["success"] = $CODE_SUCCESS;
+echo json_encode($json);
+
+?>
