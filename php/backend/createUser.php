27ad06e63d8129d83ab4317cc24be4fcef9785a3
thhesp <Thomas_spr@yahoo.de>
2014-02-17 15:36:22 +0100

    Extended Backend
    Added createUser.php

---
+++ php/backend/createUser.php
@@ -0,0 +1,92 @@
+<?php
+
+// How to use this page:
+// open it with the info about the course to create
+// as described in the database and your sessionkey:
+//
+// createUser.php?mail=mustermann@mail.com&name=mustermann&pwd=123
+//
+// required parameters are:
+// mail, name, pwd
+//
+// The answer looks as follows:
+// a json with a successcode and the course id:
+/* 
+{
+	success : 1 ,
+	data : [
+		{
+			"0":"123",
+			"id":"123",
+			"1":"mustermann@mail.com",
+			"email":"mustermann@mail.com",
+			"2":"mustermann",
+			"username":"mustermann",
+			"3":"2014-02-13 22:06:03",
+			"joindate":"2014-02-13 22:06:03",
+			"4":"2014-02-17 13:47:16",
+			"lastaction":"2014-02-17 13:47:16",
+			"5":"0",
+			"status":"0"
+		}
+	]
+}
+*/
+// for success codes see ../php/config.php
+
+// HEADER
+header('Content-Type: application/json; charset=utf-8');
+//error_reporting(0);
+include("../settings/config.php");
+include("../helpers/dbhelper.php");
+// END HEADER
+
+// PREPARE RESULT
+$json = array();
+$json["success"]=$CODE_INSUFFICIENT_PARAMETERS;
+
+if(isset($_GET["mail"])){
+	$mail = $_GET["mail"];
+}else{
+	$json["message"]="mail missing";
+	echo json_encode($json);
+	exit();
+}
+
+if(isset($_GET["name"])){
+	$name = $_GET["name"];
+}else{
+	$json["message"]="name missing";
+	echo json_encode($json);
+	exit();
+}
+
+if(isset($_GET["pwd"])){
+	$pwd = $_GET["pwd"];
+}else{
+	$json["message"]="password missing";
+	echo json_encode($json);
+	exit();
+}
+
+$db = new DBHelper();
+$data = $db->createUser($mail, $name, $pwd);
+
+if($data == false){
+	$json["success"]=$CODE_ERROR;
+	if(DBConnection::getInstance()->status == DBConfig::$dbStatus["offline"]){
+		$json["message"] = "Database error";
+	}else{
+		$json["success"] = $CODE_USER_ALREADY_EXISTS;
+		$json["message"] = "Email already exists";
+	}
+	echo json_encode($json);
+	exit();
+}
+
+$json["data"] = $data;
+
+$json["success"] = $CODE_SUCCESS;
+echo json_encode($json);
+
+?>
