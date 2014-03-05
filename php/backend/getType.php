0bc01c8a3222807be39bfac7e145170069566dc9
thhesp <Thomas_spr@yahoo.de>
2014-03-05 23:16:36 +0100

    Extended Backend
    Added getType closes #10

---
+++ php/backend/getType.php
@@ -0,0 +1,87 @@
+<?php
+
+// How to use this page:
+// open it with the info about the course to create
+// as described in the database and your sessionkey:
+//
+// getType.php?type=1
+//
+// required parameters are:
+// type
+//
+// The answer looks as follows:
+// a json with a successcode and the course id:
+/* 
+{
+	success : 1 ,
+	data : [
+		{
+			"0":"123",
+			"id":"123",
+			"1":"mustermann@mail.com",
+			"email":"mustermann@mail.com",
+			"2":"mustermann",
+			"username":"mustermann",
+			"3":"2014-02-13 22:06:03",
+			"joindate":"2014-02-13 22:06:03",
+			"4":"2014-02-17 13:47:16",
+			"lastaction":"2014-02-17 13:47:16",
+			"5":"0",
+			"status":"0"
+		}
+	]
+}
+*/
+// for success codes see ../php/config.php
+
+// HEADER
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(0);
+include("../settings/config.php");
+include("../helpers/dbhelper.php");
+// END HEADER
+
+// PREPARE RESULT
+$json = array();
+$json["success"]=$CODE_INSUFFICIENT_PARAMETERS;
+
+if(isset($_POST["type"])){
+	$_GET = $_POST;
+}
+
+if(isset($_GET["type"])){
+	$type = $_GET["type"];
+	if(is_numeric($type)){
+		$type = intval($type);
+	}
+}else{
+	$json["message"]="type missing";
+	echo json_encode($json);
+	exit();
+}
+
+$db = new DBHelper();
+
+if(isset($_GET["authkey"])){
+	$db->setAuthKey($_GET["authkey"]);
+}
+$entry = $db->getType($type);
+
+if($entry == false){
+	$json["success"]=$CODE_ERROR;
+	if(DBConnection::getInstance()->status == DBConfig::$dbStatus["offline"]){
+		$json["message"] = "Database error";
+	}else{
+		$json["success"] = $CODE_NOT_FOUND;
+		$json["message"] = "Entry not found";
+	}
+	echo json_encode($json);
+	exit();
+}
+
+$json["data"] = $entry;
+
+$json["success"] = $CODE_SUCCESS;
+echo json_encode($json);
+
+?>
