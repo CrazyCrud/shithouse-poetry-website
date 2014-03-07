bf27bc193057e4059559856719e332c6625ac077
thhesp <Thomas_spr@yahoo.de>
2014-03-07 17:38:28 +0100

    Backend Extended
    Added deleteLocation closes #31

---
+++ php/backend/deleteLocation.php
@@ -0,0 +1,73 @@
+<?php
+
+// How to use this page:
+// open it with the info about the course to create
+// as described in the database and your sessionkey:
+//
+// deleteLocation.php?authkey=xxx&locationid=1
+//
+// required parameters are:
+// authkey, locationid
+//
+// The answer looks as follows:
+// a json with a successcode and the course id:
+/* 
+{
+	success : 1 ,
+	data : true
+}
+*/
+// for success codes see ../php/config.php
+
+// HEADER
+header('Content-Type: application/json; charset=utf-8');
+error_reporting(0);
+include_once("../settings/config.php");
+include_once("../helpers/dbhelper.php");
+// END HEADER
+
+// PREPARE RESULT
+$json = array();
+$json["success"]=$CODE_INSUFFICIENT_PARAMETERS;
+
+if(isset($_POST["authkey"])){
+	$_GET = $_POST;
+}
+
+if(isset($_GET["authkey"])){
+	$key = $_GET["authkey"];
+}else{
+	$json["message"]="authkey missing";
+	echo json_encode($json);
+	exit();
+}
+
+if(isset($_GET["locationid"])){
+	$locationid = $_GET["locationid"];
+}else{
+	$json["message"]="locationid missing";
+	echo json_encode($json);
+	exit();
+}
+
+$db = new DBHelper();
+$db->setAuthKey($key);
+$status = $db->deleteLocation($locationid);
+
+if($status == false){
+	$json["success"]=$CODE_ERROR;
+	if(DBConnection::getInstance()->status == DBConfig::$dbStatus["offline"]){
+		$json["message"] = "Database error";
+	}else{
+		$json["message"] = "Entry not found";
+	}
+	echo json_encode($json);
+	exit();
+}
+
+$json["data"] = $status;
+
+$json["success"] = $CODE_SUCCESS;
+echo json_encode($json);
+
+?>
