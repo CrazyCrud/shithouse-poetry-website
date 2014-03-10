92fc810c2a96cd17528f0cf19f505a31d8775b1c
Basti <tiki_blue@web.de>
2014-03-10 17:44:08 +0100

    cookieUser in global
    moved cookieUser in global.js

--- js/global.js
+++ js/global.js
@@ -18,5 +18,12 @@ function setupRoot(){
 	}
 }
 
+function cookieUser(){
+    user = {};
+    user.id = document.cookie.replace(/(?:(?:^|.*;\s*)userid\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    user.username = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    user.admin = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1")==1;
+}
+
 
 
