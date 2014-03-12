dffec7f704a361f97b00dd581f11f4123b4cacbc
Basti <tiki_blue@web.de>
2014-03-12 16:36:51 +0100

    admin fix
    you are now an admin!

--- js/global.js
+++ js/global.js
@@ -31,7 +31,7 @@ function cookieUser(){
     user = {};
     user.id = document.cookie.replace(/(?:(?:^|.*;\s*)userid\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     user.username = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-    user.admin = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1")==1;
+    user.admin = document.cookie.replace(/(?:(?:^|.*;\s*)admin\s*\=\s*([^;]*).*$)|^.*$/, "$1")=="1";
 }
 
 function greetUser(){
