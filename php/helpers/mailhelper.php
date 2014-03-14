78e703e0920eccb2fc8ee8bb7556eb30a1ef5b3c
Basti <tiki_blue@web.de>
2014-03-14 11:04:42 +0100

    verification bugfixes

--- php/helpers/mailhelper.php
+++ php/helpers/mailhelper.php
@@ -12,9 +12,9 @@ $emailBodyContent =
 <br/>
 <p>Diese Email wurde dir geschickt, weil du dich bei <a href="http://www.latrinalia.de">http://www.latrinalia.de<a> registriert hast und nun deinen Email-Account verifizieren musst.<br/>
 Hast du dich dort nicht registriert und wei&szlig;t nicht, warum du diese Email bekommen hast ignoriere sie einfach.</p>
-Um deine Registrierung abzuschlie&szlig;en klicke bitte <a href="http://www.latrinalia.de/verify.php?key=%1">auf diesen Link</a>.
+Um deine Registrierung abzuschlie&szlig;en klicke bitte <a href="http://www.latrinalia.de/verify.php?verification=%1">auf diesen Link</a>.
 Sollte der Link aus irgendwelchen Gr&uuml;nden nicht funktionieren, kopiere einfach folgenden Text in deine Browser-Addressleiste:<br/>
-<br/>http://www.latrinalia.de/verify.php?key=%1</p>
+<br/>http://www.latrinalia.de/verify.php?verification=%1</p>
 <br/>
 <br/>
 Vielen Dank und liebe Gr&uuml;&szlig;e,<br/>
@@ -26,7 +26,7 @@ function sendVerificationMail($email, $username, $key){
 	global $emailBodyContent;
 	$url = "http://www.latrinalia.de/verify.php?verification=".$key;
 
-	$from = "noreply@latrinalia.de";
+	$from = "Latrinalia@latrinalia.de";
 	$subject = "Willkommen bei Latrinalia";
 	$message = str_replace("%1", $key, $emailBodyContent);
 
@@ -34,10 +34,7 @@ function sendVerificationMail($email, $username, $key){
 	$header .= "Content-type: text/html; charset=iso-8859-1\r\n";
 	 
 	$header .= "From: $from\r\n";
-	// $header .= "Cc: $cc\r\n";  // falls an CC gesendet werden soll
 	$header .= "X-Mailer: PHP ". phpversion();
-
-	echo $message;
 
 	mail($email,$subject,$message,$header);
 }
