4daa7fac13f8e1bfb39430a2b3ae00f06acf5b3f
CrazyCrud <Constantin.Lehenmeier@stud.uni-regensburg.de>
2014-03-19 14:02:36 +0100

    Displaying the image on detail view doesn't stumble anymore

--- js/pages/details.js
+++ js/pages/details.js
@@ -308,10 +308,14 @@ function setInfo(entry){
 function setImage(entry){
 	//set image
 	var $image = $("#image");
+	$("#image").css('visiblity', 'hidden');
 	$("#image").css('width', '100%');
 	var $entryTitle = $("#entry-title");
 	$image.attr("src", entry.images[0].largethumbnail);
 	$image.attr("title", entry.title);
+	Foundation.lib_methods.loaded($image, function(){
+		$("#image").css('visiblity', 'visible');
+	});	
 }
 
 function setRating(entry){
