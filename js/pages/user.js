b95774b7f4d1d46ac1e280cf91e6393194da9042
Basti <tiki_blue@web.de>
2014-03-21 11:08:13 +0100

    log-remove
    removed a log

--- js/pages/user.js
+++ js/pages/user.js
@@ -114,7 +114,6 @@ function drawMyFollows(follows){
 	$container.html("");
 	for(i in follows){
 		if(parseInt(follows[i].targetid)!=user.id){
-			console.log(follows[i].targetid);
 			var $user = $('<div class="following-user"><a href="user.php?id='+follows[i].targetid+'"><i class="icon-user-1"></i>'+follows[i].targetname+'</a></div>');
 			$container.append($user);
 		}
