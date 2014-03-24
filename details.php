8e279d3c7a770dafdba69fe9ecda41987b01f593
Basti <tiki_blue@web.de>
2014-03-24 13:09:57 +0100

    twitter fix
    fixed a bug where twitter-shares were displayed with twice the link

--- details.php
+++ details.php
@@ -108,7 +108,7 @@
 								<?php
 								$fulluri = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 								echo "<a title='Auf facebook teilen' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=$fulluri&t=Latrinalia - Toiletten-Schmierereien und -Graffitis'><div class='social-facebook'></div></a>";
-								echo "<a title='Auf twitter teilen' target='_blank' href='http://twitter.com/intent/tweet?source=$fulluri&text=$fulluri Latrinalia - Toiletten-Schmierereien und -Graffitis&url=$fulluri'><div class='social-twitter'></div></a>";
+								echo "<a title='Auf twitter teilen' target='_blank' href='http://twitter.com/intent/tweet?source=$fulluri&text=Latrinalia - Toiletten-Schmierereien und -Graffitis&url=$fulluri'><div class='social-twitter'></div></a>";
 								echo "<a title='Auf Google plus teilen' target='_blank' href='https://plus.google.com/share?url=$fulluri'><div class='social-gplus'></div></a>";
 								?>
 							</div>
