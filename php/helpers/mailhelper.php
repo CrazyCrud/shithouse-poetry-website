976c507e92fc540f8f94c1a6b5b6df9536f3bb33
Basti <tiki_blue@web.de>
2014-04-02 11:17:41 +0200

    mail utf-8 fix
    fixed a bug where the automatically sent mails werent utf-8 encoded

--- php/helpers/mailhelper.php
+++ php/helpers/mailhelper.php
@@ -3,7 +3,7 @@
 $adminEmails = array("bastian@latrinalia.de", "franziska@latrinalia.de", "constantin@latrinalia.de", "thomas@latrinalia.de");
 
 $emailBody = '<html>
-<head></head>
+<head><meta charset="utf-8"/></head>
 <body style="font-family:Helvetica">
 <img src="http://latrinalia.de/img/global/top-img-bw.jpg" style="width:100%"></img>
 <div>%1</div>
